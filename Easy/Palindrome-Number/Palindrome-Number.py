2d137cde5c9030240c72f5189545dbdfc9bab316
RamiHaider <ramiaboushamalah@gmail.com>
2024-08-17 00:54:25 -0400

    Completed

---
+++ Easy/Palindrome-Number/Palindrome-Number.py
@@ -0,0 +1,20 @@
+import math
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        #This part converts the number x into a list
+        #i.e., 512 becomes [5,1,2]
+        x_list = []
+        while x > 0:
+            x_list.append(x % 10)
+            x = x // 10
+        
+        #This part takes the converted list and 
+        #finds whether it is Palindrome or not
+        for i in range(0,(math.floor((len(x_list)/2)))): 
+            if x_list[i] != x_list[(i*(-1)) - 1]:
+                return False
+        return True
+
+test = Solution()
---
+++ Easy/Palindrome-Number/README.md
@@ -0,0 +1,3 @@
+# ScoreOfAString Problem
+
+# Gosh, I've done these so many times on Project Euler, but... never without turning it to a string. So that will be the different part.
